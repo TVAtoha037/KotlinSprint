cd44151dd1c557ca6ec831a2d7fbff6761804ea1
TVAtoha037 <94626626+TVAtoha037@users.noreply.github.com>
2024-06-20 09:27:42 +0300

    KS-2-4 Задание готово для проверки №2 устранены замечания

--- src/main/kotlin/lesson_2/task_4.kt
+++ src/main/kotlin/lesson_2/task_4.kt
@@ -1,22 +1,28 @@
 package org.example.lesson_2
 
+import kotlin.math.floor
+
+const val TIME_OUT: Long = 10_000L
+const val DIVISOR_PROCENT: Float = 100f
+
 fun main() {
-    var buff = false
+    var buff: Boolean = false
+    val procent: Byte = 20
 
-    var crystalOreWOBuff: Float = 7f
-    var ironOreWOBuff: Float = 11f
+    var crystalOreWOBuff: Byte = 7
+    var ironOreWOBuff: Byte = 11
 
-    val startGame = System.currentTimeMillis()
+    val startGame: Long = System.currentTimeMillis()
 
-    while((System.currentTimeMillis() - startGame) < 10_000L) {
+    while((System.currentTimeMillis() - startGame) < TIME_OUT) {
         buff = !buff
 
         if(buff){
-            val crystalBuff = crystalOreWOBuff * 0.2f
-            val ironBuff = ironOreWOBuff * 0.2f
+            val crystalBuff: Byte = floor(crystalOreWOBuff * procent / DIVISOR_PROCENT).toInt().toByte()
+            val ironBuff: Byte = floor(ironOreWOBuff * procent / DIVISOR_PROCENT) .toInt().toByte()
 
-            crystalOreWOBuff += crystalBuff
-            ironOreWOBuff += ironBuff
+            crystalOreWOBuff = (crystalOreWOBuff + crystalBuff).toByte()
+            ironOreWOBuff = (ironOreWOBuff + ironBuff).toByte()
 
             println("Iron buff: $ironBuff")
             println("Crystal buff: $crystalBuff")
