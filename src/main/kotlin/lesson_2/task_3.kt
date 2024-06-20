4d9538108a315538c29f9bf7182a7350becca749
TVAtoha037 <94626626+TVAtoha037@users.noreply.github.com>
2024-06-20 09:08:16 +0300

    KS-2-3 Задание готово для проверки №2 устранены замечания

--- src/main/kotlin/lesson_2/task_3.kt
+++ src/main/kotlin/lesson_2/task_3.kt
@@ -1,25 +1,26 @@
 package org.example.lesson_2
 
+const val DIVISOR_VALUE: Byte = 60
 fun main() {
-    val checkOutTimeHours:Int = 9
-    val checkOutTimeMinutes:Int = 39
+    val checkOutTimeHours:Byte = 9
+    val checkOutTimeMinutes:Byte = 39
 
-    val timeOfWay: Int = 457
+    val timeOfWay: Short = 457
 
-    val timeDeparture: Int = getTimeDepartureOfMinutes(checkOutTimeHours, checkOutTimeMinutes)
+    val timeDeparture: Short = getTimeDepartureOfMinutes(checkOutTimeHours, checkOutTimeMinutes)
 
-    val timeArrivalOfMinutes = timeDeparture + timeOfWay
+    val timeArrivalOfMinutes = (timeDeparture + timeOfWay).toShort()
 
     println(getTimeArrivalFromMinutes(timeArrivalOfMinutes))
 }
 
-fun getTimeDepartureOfMinutes(hour: Int, minutes: Int): Int {
-    return hour * 60 + minutes
+fun getTimeDepartureOfMinutes(hour: Byte, minutes: Byte): Short {
+    return (hour * DIVISOR_VALUE + minutes).toShort()
 }
 
-fun getTimeArrivalFromMinutes(timeArrivalOfMinutes: Int): String {
-    val hoursArrival = timeArrivalOfMinutes / 60
-    val minutesArrival = timeArrivalOfMinutes % 60
+fun getTimeArrivalFromMinutes(timeArrivalOfMinutes: Short): String {
+    val hoursArrival = timeArrivalOfMinutes / DIVISOR_VALUE
+    val minutesArrival = timeArrivalOfMinutes % DIVISOR_VALUE
 
     return String.format("%02d:%02d", hoursArrival, minutesArrival)
 }
